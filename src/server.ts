5ba90540ba3be7b1c2a809ecbe4b7be4258066ee
João <joaovictorcruzereis@gmail.com>
2020-10-16 21:59:14 -0300

    [CREATE]Validação de errors

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
 import express from 'express'
 import path from 'path';
+import 'express-async-errors';
 
-import './database/connection'
-import routes from './routes'
+import './database/connection';
+
+import routes from './routes';  
+import errorHandler from './errors/handler';
 
 //Cria aplicação
 const app = express();
@@ -11,6 +14,7 @@ app.use(express.json())
 app.use(routes)
 //Configura acesso as imagens, 'static()'
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
+app.use(errorHandler);
 
 app.listen(3333);
 
